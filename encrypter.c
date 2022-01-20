37afddcdff629eb2223506d3fa06b0b4107210cd
Dongze Li <dol005@ucsd.edu>
2022-01-19 17:26:51 -0800

    1.0 Code

--- encrypter.c
+++ encrypter.c
@@ -4,17 +4,62 @@
 
 #define MAX_LEN 256 // maximum length of a single word
 
+char rotate(char c, int n){
+    // index in ASCII
+    int charIndex = (int)c;
+
+    // upper case
+    if(charIndex >= 65 && charIndex <= 90){
+        charIndex = (charIndex + n - 65) % 26 + 65;
+    }
+
+    // lower case
+    else if(charIndex >= 97 && charIndex <= 122){
+        charIndex = (charIndex + n - 97) % 26 + 97;
+    }
+
+    // return
+    return (char)charIndex;
+}
+
 int main(int argc, char* argv[]) {
   char word[MAX_LEN];
 
-  FILE* fin = fopen( /* TODO: Read from input file */ );
+  // read file
+  FILE* fin = fopen( argv[1], "r" );
 
   if(fin == NULL) {
     perror("Could not open input file.");
     return 1;
   }
 
-  // TODO: Write your code here to implement the encryption.
+  // basic variables
+  int wordCount = 0;
+  int number = atoi(argv[2]);
+  int current;
+
+  // go over the file and set values
+  while ((current = fgetc(fin)) != EOF){
+      // space or new line
+      if(current == 10 || current == 32){
+          // print the reverse
+          for(int i = wordCount - 1; i >= 0; i--){
+              printf("%c", (char)word[i]);
+          }
+
+          // print the last ending space or new line
+          printf("%c", (char)current);
+
+          // set to zero
+          wordCount = 0;
+      }
+
+      // chars in words
+      else{
+          word[wordCount] = rotate((char)current, number);
+          wordCount++;
+      }
+  }
 
   // clean up gracefully
   fclose(fin);
